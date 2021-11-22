6597ff1a371bb35013cef8aa174f4a2adf9f4bdc
Daniela Embus <dembus@choiceworx.com>
2021-11-22 17:32:17 -0500

    feat:
    changes file directory

--- server/constants/constants.js
+++
@@ -1,9 +0,0 @@
-//They are the global variables that will be used throughout the project, it is centralized in order to have all the code burned in one place and it is easier to maintain it
-let url = "https://api.themoviedb.org/3";
-let api_key = "?api_key=f522bcbff2aca35d975625cff793a757"
-
-module.exports = {
-    url,
-    api_key
-}
-
